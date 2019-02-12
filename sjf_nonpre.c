e0adebbadfd8ea23f4a61ed7f736ddfa23346cc3
Ali Akbar <aliakbar09a@gmail.com>
2019-02-12 22:53:16 +0530

    output corrected for sjf_nonpre.c

--- sjf_nonpre.c
+++ sjf_nonpre.c
@@ -8,15 +8,16 @@ struct process
 };
 int main()
 {
-	struct process p[10], temp;
-    int ct[10],current_time,min,k,b_t,len ,sum=0,ready_queue[10], rqlen,completed, sum_waiting, pos, j;
-    printf("Enter the no. of processe : ");
+		struct process p[10], temp;
+    int ct[10],current_time,k,len ,waiting_sum=0, turnaround_sum=0, ready_queue[10]	, pos, j;
+    printf("Enter the no. of processes : ");
     scanf("%d", &len);
     for(int i=0;i<len;i++)
     {
-    	p[i].pid=i+1;
-    	p[i].ct=0;
-        scanf("%d%d", &p[i].at, &p[i].bt);
+			p[i].pid=i+1;
+			p[i].ct=0;
+			printf("Enter the arrival and burst time of p%d : ", i+1);
+			scanf("%d%d", &p[i].at, &p[i].bt);
     }
     for(int i=0;i<len;i++)
     {
@@ -31,8 +32,6 @@ int main()
 		}
     }
     current_time = p[0].at;
-    rqlen = 0;
-    completed=0;
     for(int i=0;i<len;i++)
     {
     	j=i+1;
@@ -57,18 +56,37 @@ int main()
     for(int i=0;i<len;i++)
     {
     	for(int j=i+1;j<len;j++)
-		{
-			if(p[i].pid > p[j].pid)
 			{
-				temp=p[i];
-				p[i]=p[j];
-				p[j]=temp;
+				if(p[i].pid > p[j].pid)
+				{
+					temp=p[i];
+					p[i]=p[j];
+					p[j]=temp;
+				}
 			}
-		}
     }
     for(int i=0;i<len;i++)
     {
-        printf(" pid=%d at=%d bt=%d ct=%d\n", p[i].pid, p[i].at, p[i].bt, p[i].ct);
+        printf("p%d arrival time =%d, burst time=%d, completion time=%d\n", p[i].pid, p[i].at, p[i].bt, p[i].ct);
     }
-    
+		for(int i=0;i<len;i++)
+    {
+    	for(int j=i+1;j<len;j++)
+			{
+				if(p[i].ct > p[j].ct)
+				{
+					temp=p[i];
+					p[i]=p[j];
+					p[j]=temp;
+				}
+			}
+    }
+		printf("\nGantt Chart\n0");
+		if(p[0].at != 0)
+			printf("##%d", p[0].at);
+		for(int i=0;i<len;i++)
+    {
+        printf(" p%d %d", p[i].pid, p[i].ct);
+    }
+		printf("\n");
 }
